c3a7a11da0216c20d59e96a6fbcb390773513467
Daniel Chudnov <dchud@gwu.edu>
2012-06-21 14:01:12 -0400

    changing order of oclc urls, fixes #51

--- lp/lp/urls.py
+++ lp/lp/urls.py
@@ -24,10 +24,10 @@ urlpatterns += patterns('ui.views',
     url(r'^item/(?P<bibid>\.?b?\d{6,8}).json$', 'item_json', name='item_json'),
     url(r'^issn/(?P<issn>\d{4}-?\d{3}[0-9Xx])$', 'issn', name='issn'),
     url(r'^isbn/(?P<isbn>[0-9-xX]+)$', 'isbn', name='isbn'),
-    url(r'^oclc/(?P<oclc>\d{8,9})$', 'oclc', name='oclc'),
     url(r'^oclc/\(OCoLC\)oc[mn](?P<oclc>\d{8,10})$', 'oclc', name='oclc'),
     url(r'^oclc/\(OCoLC\)(?P<oclc>\d{8,10})$', 'oclc', name='oclc'),
     url(r'^oclc/oc[mn](?P<oclc>\d{8,10})$', 'oclc', name='oclc'),
     url(r'^oclc/\(Safari\)(?P<oclc>\d{8,10})$', 'oclc', name='oclc'),
+    url(r'^oclc/(?P<oclc>\d{8,10})$', 'oclc', name='oclc'),
 )
 
