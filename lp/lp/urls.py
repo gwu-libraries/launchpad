d4266b2bbb8524c6c8ce940aa6db80557b151482
Joshua <jngomez@gwu.edu>
2012-07-09 13:47:32 -0400

    Grouping results by school. Removed two letter code from location display. Added temp location and status displays. Changed OCLC lookup from index cdoe 035A to 0350

--- lp/lp/urls.py
+++ lp/lp/urls.py
@@ -6,8 +6,8 @@ handler500 = 'ui.views.error500'
 
 urlpatterns = patterns('ui.views',
     url(r'^$', 'home', name='home'),
-    url(r'^item/(?P<bibid>\.?b?\d{6,8})$', 'item', name='item'),
-    url(r'^item/(?P<bibid>\.?b?\d{6,8}).json$', 'item_json', name='item_json'),
+    url(r'^item/(?P<bibid>\.?b?\d{2,8})$', 'item', name='item'),
+    url(r'^item/(?P<bibid>\.?b?\d{2,8}).json$', 'item_json', name='item_json'),
     url(r'^issn/(?P<issn>\d{4}-?\d{3}[0-9Xx])$', 'issn', name='issn'),
     url(r'^isbn/(?P<isbn>[0-9-xX]+)$', 'isbn', name='isbn'),
     url(r'^oclc/\(OCoLC\)oc[mn](?P<oclc>\d{7,10})$', 'oclc', name='oclc'),
