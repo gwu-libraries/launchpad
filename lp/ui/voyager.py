d2fb4bfc4c089f1d27e2b690b8aed63bee5cf293
Daniel Chudnov <dchud@gwu.edu>
2012-07-17 20:43:43 -0400

    checking for empty std_nums; fixes #104

--- lp/ui/voyager.py
+++ lp/ui/voyager.py
@@ -98,13 +98,14 @@ AND bib_master.suppress_in_opac='N'"""
         if bib.get(num_type.upper(),''):
             norm_set, disp_set = set(), set()
             std_nums = get_related_std_nums(bib['BIB_ID'], num_type)
-            norm, disp = zip(*std_nums)
-            norm_set.update(norm)
-            disp_set.update([num.strip() for num in disp])
-            bib['NORMAL_%s_LIST' % num_type.upper()] = list(norm_set)
-            bib['DISPLAY_%s_LIST' % num_type.upper()] = list(disp_set)
-            # use std nums to get related bibs
-            bibids.update(get_related_bibids(norm, num_type))
+            if std_nums:
+                norm, disp = zip(*std_nums)
+                norm_set.update(norm)
+                disp_set.update([num.strip() for num in disp])
+                bib['NORMAL_%s_LIST' % num_type.upper()] = list(norm_set)
+                bib['DISPLAY_%s_LIST' % num_type.upper()] = list(disp_set)
+                # use std nums to get related bibs
+                bibids.update(get_related_bibids(norm, num_type))
     bib['BIB_ID_LIST'] = list(bibids)
     return bib
     
