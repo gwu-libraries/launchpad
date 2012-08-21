659335ba92195cf5b2edf87238943bc150a8d8cc
Joshua <jngomez@gwu.edu>
2012-08-21 13:04:38 -0400

    Created setter and getter functionality through the dot operator for the Bib class. Added gtbib and gmbib functionality to wrlc bibid lookup

--- lp/ui/catalogs/wrlc.py
+++ lp/ui/catalogs/wrlc.py
@@ -12,87 +12,79 @@ from ui.templatetags.launchpad_extras import clean_isbn, clean_oclc, clean_issn
 from ui.models import Bib
 
 
+'''
+Primary function for retrieving a record from the Voyager DB using the Bib
+object type
+'''
 def bib(bibid, expand=True):
     query = """
-SELECT bib_text.bib_id AS bibid, 
-       title, 
-       author, 
-       edition, 
-       isbn, 
-       issn, 
-       imprint, 
+SELECT bib_text.bib_id AS bibid,
+       title,
+       author,
+       edition,
+       isbn,
+       issn,
+       imprint,
        publisher, 
-       pub_place AS publisher_date,
-       publisher_date,
-       bib_format AS format_code,
-       language AS language_code,
-       library_name AS library_code,
+       pub_place AS pubplace,
+       publisher_date AS pubyear,
+       bib_format AS formatcode,
+       language AS langcode,
+       library_name AS libcode,
        network_number AS oclc,
-       wrlcdb.GetBibTag(%s,'856') AS marc856,
-       RTRIM(wrlcdb.GetMarcField(%s,0,0,'245','','',1)) AS marc245,
-       wrlcdb.GetAllBibTag(%s, '880', 1) AS marc880,
-       wrlcdb.GetBibTag(%s, '006') AS marc006,
-       wrlcdb.GetBibTag(%s, '007') AS marc007,
-       wrlcdb.GetBibTag(%s, '008') AS marc008,
-       wrlcdb.GetAllBibTag(%s, '700', 1) AS marc700,
-       wrlcdb.GetAllBibTag(%s, '710', 1) AS marc710,
-       wrlcdb.GetAllBibTag(%s, '711', 1) AS marc711
+       wrlcdb.getBibBlob(%s) AS marcblob
 FROM bib_text, bib_master, library
 WHERE bib_text.bib_id=%s
 AND bib_text.bib_id=bib_master.bib_id
 AND bib_master.library_id=library.library_id
 AND bib_master.suppress_in_opac='N'"""
     cursor = connection.cursor()
-    cursor.execute(query, [bibid]*10)
+    cursor.execute(query, [bibid]*2)
     data = _make_dict(cursor, first=True)
-    bib = Bib()
-    for field in data:
-        bib[field.lower()] = data[field]
+    raw_marc = str(data.pop('marcblob'))
+    bib = Bib(metadata=data, raw_marc=raw_marc)
     return bib
 
 
+'''
+This function takes various standard numbers and returns a WRLC bibid number.
+It can accept: isbn, issn, oclc, gtbibid, gmbibid
+If there are multiple bibids it gives preference to the preferred library (as
+specified in the settings file)
+'''
 def bibid(num, num_type):
-    if num_type in ('gtbibid', 'gmbibid'):
-        return _convert_to_wrlc_bibid(bibid=num, bibid_type=num_type)
     num = _normalize_num(num, num_type)
     query = """
-SELECT bib_index.bib_id, bib_master.library_id, library.library_name
+SELECT bib_index.bib_id AS  bibid,
+       library.library_name AS library_code
 FROM bib_index, bib_master, library
 WHERE bib_index.index_code IN %s
 AND bib_index.normal_heading = '%s'
 AND bib_index.bib_id=bib_master.bib_id
 AND bib_master.library_id=library.library_id"""
-    query = query % (_in_clause(settings.INDEX_CODES[num_type]), num)
+    if num_type == 'gtbibid':
+        index_codes = "('907A')"
+    elif num_type == 'gmbibid':
+        index_codes = "('035A')"
+        query += "\nAND bib_index.normal_heading=bib_index.display_heading"
+    else:
+        index_codes = _in_clause(settings.INDEX_CODES[num_type])
+    query = query % (index_codes, num)
     cursor = connection.cursor()
     cursor.execute(query, [])
     bibs = _make_dict(cursor)
-    for bib in bibs:
-        if bib['LIBRARY_NAME'] == settings.PREF_LIB:
-            return bib['BIB_ID']
-    return bibs[0]['BIB_ID'] if bibs else None
-
-
-def _convert_to_wrlc_bibid(bibid, bibid_type):
-    query = """
-SELECT bib_index.bib_id
-FROM bib_index
-WHERE bib_index.index_code = '%s'
-AND bib_index.normal_heading = '%s'"""
-    if bibid_type == 'gtbibid':
-        index_code = '907A'
-    elif bibid_type == 'gmbibid':
-        index_code = '035A'
-        query += "\nAND bib_index.normal_heading=bib_index.display_heading"
-    query = query % (index_code, bibid)
-    print query
-    cursor = connection.cursor()
-    cursor.execute(query, [])
-    return cursor.fetchone()[0] 
+    if bibs:
+        for bib in bibs:
+            if bib['library_code'] == settings.PREF_LIB:
+                return bib['bibid']
+        return bibs[0]['bibid']
 
 
 def related_bibids(num_list, num_type):
     query = """
-SELECT DISTINCT bib_index.bib_id, bib_index.display_heading, library.library_name
+SELECT DISTINCT bib_index.bib_id, 
+       bib_index.display_heading, 
+       library.library_name
 FROM bib_index, library, bib_master
 WHERE bib_index.bib_id=bib_master.bib_id
 AND bib_master.library_id=library.library_id
@@ -108,7 +100,9 @@ AND bib_index.normal_heading IN (
         )
     )
 ORDER BY bib_index.bib_id"""
-    query = query % (_in_clause(settings.INDEX_CODES[num_type]), _in_clause(settings.INDEX_CODES[num_type]), _in_clause(num_list))
+    query = query % (_in_clause(settings.INDEX_CODES[num_type]), 
+                     _in_clause(settings.INDEX_CODES[num_type]), 
+                     _in_clause(num_list))
     cursor = connection.cursor()
     cursor.execute(query, [])
     results = _make_dict(cursor)
@@ -118,10 +112,54 @@ ORDER BY bib_index.bib_id"""
     return [dict([(k, row[k]) for k in output_keys]) for row in results]
 
 
+def holdings(bib):
+    query = """
+SELECT bib_mfhd.bib_id,
+       mfhd_master.mfhd_id,
+       mfhd_master.location_id,
+       mfhd_master.display_call_no,
+       location.location_display_name,
+       library.library_name
+FROM bib_mfhd INNER JOIN mfhd_master ON bib_mfhd.mfhd_id = mfhd_master.mfhd_id,
+     location,
+     library,
+     bib_master
+WHERE mfhd_master.location_id=location.location_id
+AND bib_mfhd.bib_id IN %s
+AND mfhd_master.suppress_in_opac !='Y'
+AND bib_mfhd.bib_id = bib_master.bib_id
+AND bib_master.library_id = library.library_id
+ORDER BY library.library_name"""
+    query = query % _in_clause([b['BIB_ID'] for b in bib['BIB_ID_LIST']])
+    cursor = connection.cursor()
+    cursor.execute(query, [])
+    return _make_dict(cursor)
+
+
+def bibblob(bibid):
+    query = """
+SELECT wrlcdb.getBibBlob(%s) AS bibblob
+FROM bib_text
+WHERE bib_text.bib_id = %s"""
+    cursor = connection.cursor()
+    cursor.execute(query, [bibid, bibid])
+    return cursor.fetchone()
+
+
+def mfhdblob(mfhdid):
+    query = """
+SELECT wrlcdb.getMFHDBlob(%s) AS mfhdblob
+FROM mfhd_master
+WHERE mfhd_id = %s"""
+    cursor = connection.cursor()
+    cursor.execute(query, [mfhdid]*2)
+    return cursor.fetchone()
+
+
 def _make_dict(cursor, first=False):
     desc = cursor.description
     mapped = [
-        dict(zip([col[0] for col in desc], row))
+        dict(zip([col[0].lower() for col in desc], row))
         for row in cursor.fetchall()
     ]
     # strip string values of trailing whitespace
@@ -142,7 +180,7 @@ def _normalize_num(num, num_type):
     if num_type == 'isbn':
         return clean_isbn(num)
     elif num_type == 'issn':
-        return num.replace('-',' ')
+        return num.replace('-', ' ')
     elif num_type == 'oclc':
         return clean_oclc(num)
     return num
--- lp/ui/models.py
+++ lp/ui/models.py
@@ -1,96 +1,203 @@
 import pycountry
 
-from django.db import models
 from django.conf import settings
+
+import pymarc
 
 
 class Bib(object):
 
-    def __init__(self):
-    
-        self.fields = {}
-        self.holdings = []
-    
-    def __getitem__(self, key):
+    def __init__(self, metadata={}, raw_marc=None, holdings=[]):
+
+        '''
+        self.metadata is used for indexed metadata in catalog databases as well
+        as metadata gathered from various bibliographic APIs such as Worldcat
+        and Google Books. It should be a dictionary using the following key
+        names (note that no field is required, this list is provided only to
+        ensure the key names match:
+            bibid           (catalog record)
+            related_bibids  (list of related catalog records)
+            title
+            author          (primary)
+            addedentries    (additional authors and editors, etc.)
+            edition
+            isbn            (the primary isbn associated with this item)
+            related_isbns   (a list of related isbns)
+            issn            (primary)
+            related_issns   (related)
+            oclc
+            publisher
+            pubplace
+            pubyear
+            langcode
+            libcode
+            formatcode
+        '''
+        super(Bib, self).__setattr__('metadata', metadata)
+        super(Bib, self).__setattr__('holdings', holdings)
+        super(Bib, self).__setattr__('marc', raw_marc)
+        if raw_marc:
+            self.load_marc(raw_marc)
+
+    '''
+    The following methods allow for convenient access to bibliographic metadata
+    using the dot operator (ex: mybib.title). This will first attempt to use a
+    local method of producing the metadata on the fly if one exists, otherwise
+    it will use the value stored in the indexed metadata dictionary. If no data
+    exists it returns None.
+    '''
+    def __getattr__(self, name):
+        if name.startswith('get_'):
+            return None
+        if getattr(self, 'get_' + name, None) is not None:
+            return getattr(self, 'get_' + name)()
+        if getattr(self.marc, name, None) is not None:
+            return getattr(self.marc, name)()
         try:
-            return self.fields[key]
+            return self.metadata[name]
         except:
             return None
-            
-    def __setitem__(self, key, value):
-        self.fields[key] = value
-        
-    def title(self, raw_marc=False):
-        if raw_marc:
-            return self['marc245']
-        try:
-            return ' '.join([subfield[1:].rstrip(' /') for subfield in self.fields['marc245'].split('$')[1:] if subfield[0] in ('a','b')])
-        except:
-            return self['title']
-    
-    def bibids(self, include_primary=True):
-        bibids = [self['bibid']] if self['bibid'] and include_primary else []
-        if self['related_bibids']:
-            bibids.extend(self['related_bibids'])
-        return bibids
-    
-    def authors(self, include_primary=True):
-        authlist = [self['author']] if self['author'] and include_primary else []
-        for field in ('marc700', 'marc710', 'marc711'):
-            if self[field]:
-                authlist.extend([auth.rstrip(' .') for auth in self[field].split(' // ')])
-        return authlist
 
-    def isbns(self, include_primary=True):
-        isbns = [self['isbn']] if self['isbn'] and include_primary else []
-        if self['related_isbns']:
-            isbns.extend(self['related_isbns'])
-        return isbns
-    
-    def issns(self, include_primary=True):
-        issns = [self['issn']] if self['issn'] and include_primary else []
-        if self['related_issns']:
-            issns.extend(self['related_issns'])
-        return issns
-    
-    def oclcs(self, include_primary=True):
-        oclcs = [self['isbn']] if self['isbn'] and include_primary else []
-        if self['related_oclcs']:
-            oclcs.extend(self['related_oclcs'])
-        return oclcs
-    
-    def language(self):
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            super(Bib, self).__setattr__(name, value)
+        else:
+            fname = 'set_' + name
+            function = getattr(self, fname, None)
+            if function is not None:
+                function(value)
+            else:
+                self.metadata[name] = value
+    '''
+    Initial loading functions, provided as conveniences. This data can be
+    loaded during object creation. These functions overwrite previous data.
+    '''
+    def load_marc(self, raw_marc):
         try:
-            language = pycountry.languages.get(bibliographic=self['language_code'])
+            self.marc = pymarc.record.Record(data=raw_marc)
+        except:
+            self.marc = None
+            #TODO: throw error
+
+    def load_holdings(self, holdings):
+        self.holdings = []
+        self.add_holdings(holdings)
+
+    def load_metadata(self, metadata):
+        try:
+            for key in metadata:
+                self.metadata[key] = metadata[key]
+        except:
+            self.metadata = {}
+            #TODO: throw error
+
+    '''
+    getter, setter, and adder functions
+    The getter and setter functions are called simply using the object's dot
+    operator (ex: mybib.bibids or mybib.isbns = ['1234','3450','6294']).
+    The adder functions ensure no duplicates when adding to a list attribute.
+    Note that list attributes like isbn and bibid often have two components:
+    the primary number and the list of related numbers. Thus there are three
+    methods of accessing the combination of the two:
+        mybib.bibid             (primary only)
+        mybib.bibids            (primary at front of list)
+        mbbib.related_bibids    (list does not include primary)
+    '''
+    def add_holdings(self, new_holdings):
+        #TODO: add type checking
+        ids = [holding.id for holding in self.holdings]
+        for new_holding in new_holdings:
+            if new_holding.id not in ids:
+                self.holdings.append(new_holding)
+                ids.append(new_holding.id)
+
+    def get_bibids(self):
+        bibids = [self.bibid] if self.bibid else []
+        bibids.extend(self.metadata.get('related_bibids', []))
+        return bibids
+
+    def add_bibids(self, new_bibids):
+        new_bibids = [nb for nb in new_bibids if nb not in self.get_bibids()]
+        self.metadata['related_bibids'].extend(new_bibids)
+
+    def add_addedentries(self, new_names):
+        new_names = [nn for nn in new_names if nn not in self.get_added_entries() and not nn == self.author]
+        self.metadata['addedentries'].extend(new_names)
+
+    def get_authors(self):
+        authors = [self.author] if self.author else []
+        authors.extend(self.get_added_entries())
+        return authors
+
+    def add_isbns(self, new_isbns):
+        isbns = self.metadata.get('related_isbns', [])
+        new_isbns = [ni for ni in new_isbns if not ni == self.isbn and ni not in isbns]
+        self.metadata['related_isbns'] = isbns.extend(new_isbns)
+
+    def get_isbns(self):
+        isbns = [self.isbn] if self.isbn else []
+        isbns.extend(self.metadata.get('related_isbns', []))
+        return isbns
+
+    def add_issns(self, new_issns):
+        issns = self.metadata.get('related_issns', [])
+        new_issns = [ni for ni in new_issns if not ni == self.issn and ni not in issns]
+        self.metadata['related_issns'] = issns.extend(new_issns)
+
+    def get_issns(self):
+        issns = [self.issn] if self.issn else []
+        issns.extend(self.metadata.get('related_issns', []))
+        return issns
+
+    def get_language(self):
+        try:
+            language = pycountry.languages.get(bibliographic=self.langcode)
             return language.name
         except:
-            return self['language_code']
-            
-    def library(self):
+            return self.langcode
+
+    def get_library(self):
         try:
-            return settings.LIB_LOOKUP[self['library_code']]
+            return settings.LIB_LOOKUP[self.libcode]
         except:
-            return self['library_code']
-    
-    def imprint(self):
-        if self['imprint']:
-            return self['imprint']
-        return ' '.join([self[f].strip() for f in ('publisher_place', 'publisher', 'publisher_date') if self[f]])
-    
-    def altscripts(self, asdict=False):
+            return self.libcode
+
+    def get_altscripts(self, asdict=False):
+        alts = {}
+        if self.pymarc:
+            fields = self.pymarc.get_fields('880')
+            for field in fields:
+                if field['6'] == '245':
+                    alts['title'] = ' '.join(field.get_subfields('a', 'b'))
+                elif field['6'] == '260':
+                    alts['publisher'] = field['b']
+                    alts['pubdate'] = field['c']
+                elif field['6'] in ('100', '110', '111'):
+                    alts['author'] = field['a']
+                elif field['6'] in ('700', '710', '711', '720', '730', '740',
+                    '752', '753', '754', '790', '791', '792', '793', '796',
+                    '797', '798', '799'):
+                    if not alts.get('addedentries', []):
+                        alts['addedentries'] = []
+                    alts['addedentries'].append(field['a'])
+        return alts
+
+    def get_openurl(self):
+        pass
+
+
+class Holding(object):
+
+    def __init__(self):
+
+        self.metadata = {}
+        self.items = []
+
+    def __getitem__(self, key):
         try:
-            tags = self['marc880'].split(' // ')
-            if asdict:
-                d = {}
-                for tag in tags:
-                            d[tag[:3]] = tag[8:].rstrip()
-            else:
-                d = [tag[8:].rstrip() for tag in tags]
-            return d
+            return self.metadata[key]
         except:
             return None
-    
-    def openurl(self):
-        pass
-        
 
+    def __setitem__(self, key, value):
+        self.metadata[key] = value
