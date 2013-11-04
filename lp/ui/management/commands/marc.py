68809f47dc1a82f765e463fe483e7a6a66aa3278
Ed Summers <ehs@pobox.com>
2013-11-04 05:19:13 -0500

    added a management command to easily get marc as marc21/json/xml for a given bibid

--- lp/ui/management/commands/marc.py
+++ lp/ui/management/commands/marc.py
@@ -1,5 +1,5 @@
-import pprint
-
+from optparse import make_option
+from pymarc.marcxml import record_to_xml
 from django.core.management.base import BaseCommand
 
 from ui import voyager
@@ -7,9 +7,21 @@ from ui import voyager
 
 class Command(BaseCommand):
     args = '<bibid>'
-    help = 'get marc record for a given bibid'
+    help = 'get the marc record for a given bibid'
 
-    def handle(self, *args, **options):
-        if len(args) >= 1:
-            record = voyager.get_marc_blob(args[0])
+    format = make_option('--format', 
+                         action='store',
+                         dest='format',
+                         default='marc',
+                         help='format for record: json, marc, xml')
+
+    option_list = BaseCommand.option_list + (format,)
+
+    def handle(self, bibid, **options):
+        record = voyager.get_marc_blob(bibid)
+        if options['format'] == 'json':
             print record.as_json(indent=2)
+        elif options['format'] == 'xml':
+            print record_to_xml(record)
+        else:
+            print record.as_marc21()
