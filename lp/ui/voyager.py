5dc02e78881fd3f5a39dc770da45df8a027ef051
Raza Gilani <sgilani@email.gwu.edu>
2014-01-08 13:58:03 -0500

    fixed the issue in removing duplicate items, fixes #356

--- lp/ui/voyager.py
+++ lp/ui/voyager.py
@@ -605,27 +605,14 @@ def remove_duplicate_items(i, items):
     #check if the item has already been processed
     if 'REMOVE' in items[i]:
         return
-    if 'ITEM_STATUS_DATE' in items[i]:
-        if items[i]['ITEM_STATUS_DATE'] is None:
-            items[i]['REMOVE'] = True
     j = i + 1
     while j < len(items):
         if items[i]['ITEM_ID'] == items[j]['ITEM_ID']:
-            if 'ITEM_STATUS_DATE' in items[i]:
-                if items[i]['ITEM_STATUS_DATE'] is None:
-                    items[i]['REMOVE'] = True
-            if 'ITEM_STATUS_DATE' in items[j]:
-                if items[j]['ITEM_STATUS_DATE'] is None:
-                    items[j]['REMOVE'] = True
-            if ('ITEM_STATUS_DATE' in items[i] and
-                    'ITEM_STATUS_DATE' in items[j]):
-                if (items[i]['ITEM_STATUS_DATE'] is not None and
-                        items[j]['ITEM_STATUS_DATE'] is not None):
-                    if (items[i]['ITEM_STATUS_DATE'] >
-                            items[j]['ITEM_STATUS_DATE']):
-                        items[j]['REMOVE'] = True
-                    else:
-                        items[i]['REMOVE'] = True
+            if (items[i]['ITEM_STATUS_DATE'] >
+                items[j]['ITEM_STATUS_DATE']):
+                items[j]['REMOVE'] = True
+            else:
+                items[i]['REMOVE'] = True
         j = j + 1
 
 
