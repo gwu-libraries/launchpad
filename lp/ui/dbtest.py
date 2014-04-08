fd5a4a1bbf03099d883767c3271a7419dde9459b
Ed Summers <ehs@pobox.com>
2014-04-08 04:42:26 -0400

    added test for availability

--- lp/ui/dbtest.py
+++ lp/ui/dbtest.py
@@ -14,3 +14,15 @@ class DbTests(TestCase):
     def test_book(self):
         i = get_item('12278722')
         self.assertEqual(i['@type'], 'Book')
+
+    def test_availability(self):
+        a = get_availability('4467824')
+        self.assertEqual(len(a), 1)
+
+        o = a[0]
+        self.assertEqual(o, 'Offer')
+        self.assertEqual(o['seller'], 'Georgetown University')
+        self.assertEqual(o['availabilityAtOrFrom'], 'Lauinger stacks')
+        self.assertEqual(o['sku'], 'PR6019.O9 F45 1939')
+        self.assertEqual(o['status'], 'http://purl.org/goodrelations/v1#LeaseOut')
+        self.assertEqual(o['serialNumber'], '8988479')
