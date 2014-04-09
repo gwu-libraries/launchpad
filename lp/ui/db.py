f3d53b1e4fa90a45bb56db70b32a004fd83210ce
Ed Summers <ehs@pobox.com>
2014-04-09 06:17:34 -0400

    filled out get_availability some more

--- lp/ui/db.py
+++ lp/ui/db.py
@@ -11,6 +11,9 @@ from django.conf import settings
 
 
 def get_item(bibid):
+    """
+    Get JSON-LD for a given bibid.
+    """
     marc = get_marc(bibid)
     return {
         '@type': 'Book',
@@ -19,6 +22,9 @@ def get_item(bibid):
 
 
 def get_marc(bibid):
+    """
+    Get pymarc.Record for a given bibid.
+    """
     query = "SELECT wrlcdb.getBibBlob(%s) AS marcblob from bib_master"
     cursor = connection.cursor()
     cursor.execute(query, [bibid])
@@ -27,49 +33,50 @@ def get_marc(bibid):
     record = pymarc.record.Record(data=raw_marc)
     return record
 
-def get_availability(bibid):
 
-    # old query
-    """        
-        SELECT mfhd_master.mfhd_id, mfhd_master.location_id, 
-               mfhd_master.display_call_no, location.location_display_name,
-               library.library_name, location.location_name
-        FROM bib_mfhd
-        INNER JOIN mfhd_master ON bib_mfhd.mfhd_id = mfhd_master.mfhd_id,
-             location, library, bib_master
-        WHERE mfhd_master.location_id = location.location_id
-        AND bib_mfhd.bib_id = %s
-        AND mfhd_master.suppress_in_opac != 'Y'
-        AND bib_mfhd.bib_id = bib_master.bib_id
-        AND bib_master.library_id=library.library_id
-        ORDER BY library.library_name
+def get_availability(bibid):
+    """
+    Get availability information as JSON-LD for a given bibid.
     """
 
-    # TODO: left outer join on item so we can get library name for GT, GM, etc
-    query = """
-SELECT DISTINCT display_call_no, item_status_desc, item_status.item_status,
-       permLocation.location_display_name as PermLocation,
-       tempLocation.location_display_name as TempLocation,
-       mfhd_item.item_enum, mfhd_item.chron, item.item_id, item_status_date,
-       to_char(CIRC_TRANSACTIONS.CHARGE_DUE_DATE, 'mm-dd-yyyy') AS DUE,
-       library.library_display_name
-FROM bib_master
-JOIN library ON library.library_id = bib_master.library_id
-JOIN bib_mfhd ON bib_master.bib_id = bib_mfhd.bib_id
-JOIN mfhd_master ON mfhd_master.mfhd_id = bib_mfhd.mfhd_id
-JOIN mfhd_item on mfhd_item.mfhd_id = mfhd_master.mfhd_id
-JOIN item ON item.item_id = mfhd_item.item_id
-JOIN item_status ON item_status.item_id = item.item_id
-JOIN item_status_type ON
-    item_status.item_status = item_status_type.item_status_type
-JOIN location permLocation ON permLocation.location_id = item.perm_location
-JOIN library ON bib_master.library_id = library.library_id
-LEFT OUTER JOIN location tempLocation ON
-    tempLocation.location_id = item.temp_location
-LEFT OUTER JOIN circ_transactions ON item.item_id = circ_transactions.item_id
-WHERE bib_master.bib_id = %s
-AND mfhd_master.suppress_in_opac != 'Y'
-ORDER BY PermLocation, TempLocation, item_status_date desc"""
+    query = \
+        """
+        SELECT DISTINCT 
+          display_call_no, 
+          item_status_desc,
+          item_status.item_status,
+          permLocation.location_display_name as PermLocation,
+          tempLocation.location_display_name as TempLocation,
+          mfhd_item.item_enum, 
+          mfhd_item.chron, 
+          item.item_id,
+          item_status_date,
+          to_char(CIRC_TRANSACTIONS.CHARGE_DUE_DATE, 'mm-dd-yyyy') AS DUE,
+          library.library_display_name
+        FROM bib_master
+        JOIN library ON library.library_id = bib_master.library_id
+        JOIN bib_mfhd ON bib_master.bib_id = bib_mfhd.bib_id
+        JOIN mfhd_master ON mfhd_master.mfhd_id = bib_mfhd.mfhd_id
+        JOIN library ON bib_master.library_id = library.library_id
+        LEFT OUTER JOIN mfhd_item
+          ON mfhd_item.mfhd_id = mfhd_master.mfhd_id
+        LEFT OUTER JOIN item
+          ON item.item_id = mfhd_item.item_id
+        LEFT OUTER JOIN item_status
+          ON item_status.item_id = item.item_id
+        LEFT OUTER JOIN item_status_type
+          ON item_status.item_status = item_status_type.item_status_type
+        LEFT OUTER JOIN location permLocation
+          ON permLocation.location_id = item.perm_location
+        LEFT OUTER JOIN location tempLocation
+          ON tempLocation.location_id = item.temp_location
+        LEFT OUTER JOIN circ_transactions
+          ON item.item_id = circ_transactions.item_id
+        WHERE bib_master.bib_id = %s
+        AND mfhd_master.suppress_in_opac != 'Y'
+        ORDER BY PermLocation, TempLocation, item_status_date desc
+        """
+
     cursor = connection.cursor()
     cursor.execute(query, [bibid])
 
@@ -79,12 +86,59 @@ ORDER BY PermLocation, TempLocation, item_status_date desc"""
         a = {
             '@type': 'Offer',
             'seller': seller,
-            'availabilityAtOrFrom': row[3],
+            'availabilityAtOrFrom': _normalize_location(row[3]),
             'sku': row[0],
-            'status': row[2],
-            'serialNumber': row[7],
+            'status': _normalize_status(row[1]),
+            'serialNumber': str(row[7]),
         }
         results.append(a)
 
     return results
 
+def _normalize_status(status_id):
+    """
+    This function will turn one of the standard item status codes 
+    into a GoodRelations URI: 
+
+    http://www.w3.org/community/schemabibex/wiki/Holdings_via_Offer
+
+    Here is a snapshot in time of item_status_ids, their description, 
+    and count:
+
+      1 Not Charged                  5897029
+      2 Charged                      1832241
+      3 Renewed                        39548
+     17 Withdrawn                      26613
+      4 Overdue                        22966
+     14 Lost--System Applied           16687
+     12 Missing                        15816
+     19 Cataloging Review              15584
+     20 Circulation Review             11225
+      9 In Transit Discharged           7493
+     13 Lost--Library Applied           7262
+     11 Discharged                      2001
+     18 At Bindery                      1603
+     15 Claims Returned                  637
+     16 Damaged                          525
+     22 In Process                       276
+      6 Hold Request                      39
+     10 In Transit On Hold                24
+      8 In Transit                        23
+      5 Recall Request                    17
+      7 On Hold                            6
+     24 Short Loan Request                 2
+
+    """
+
+    # TODO: more granularity needed?
+    if status_id == 1:
+        return 'http://schema.org/InStock'
+    else:
+        return 'http://schema.org/OutOfStock'
+
+
+def _normalize_location(location):
+    if not location:
+        return None
+    parts = location.split(': ', 1)
+    return parts.pop().capitalize()
--- lp/ui/dbtest.py
+++ lp/ui/dbtest.py
@@ -17,7 +17,6 @@ class DbTests(TestCase):
 
     def test_availability(self):
         a = get_availability('5769326')
-        print a
         self.assertEqual(len(a), 1)
 
         o = a[0]
@@ -25,5 +24,18 @@ class DbTests(TestCase):
         self.assertEqual(o['seller'], 'George Washington')
         self.assertEqual(o['availabilityAtOrFrom'], 'Gelman stacks')
         self.assertEqual(o['sku'], 'PR6019.O9 F5 1999')
-        self.assertEqual(o['status'], 'http://purl.org/goodrelations/v1#LeaseOut')
-        self.assertEqual(o['serialNumber'], '999')
+        self.assertTrue(o['status'] in [
+            'http://schema.org/InStock',
+            'http://schema.org/OutOfStock'
+        ])
+        self.assertEqual(o['serialNumber'], '3927007')
+
+    def test_availability_georgetown(self):
+        a = get_availability('4218864')
+        self.assertEqual(len(a), 1)
+
+        o = a[0]
+        self.assertEqual(o['@type'], 'Offer')
+        self.assertEqual(o['seller'], 'Georgetown')
+        # TODO: this should be something else once z39.50 lookup is working
+        self.assertEqual(o['sku'], None)
