7b6ffbf2998c0ac142b567decb5ad675325835ba
Michael Cummings <michaelc@gwu.edu>
2015-01-09 10:40:17 -0500

    updated test for isbn 9784062879248. Fixes #929.

--- lp/ui/tests/summon_test.py
+++ lp/ui/tests/summon_test.py
@@ -57,7 +57,6 @@ class SummonTests(unittest.TestCase):
         self.assertEqual(i['datePublished'], '2000')
         self.assertEqual(i['thumbnailUrl'], 'http://covers-cdn.summon.serialssolutions.com/index.aspx?isbn=9781573870993/mc.gif&client=summon&freeimage=true')
         self.assertEqual(i['bookEdition'], '1. print')
-        self.assertEqual(len(i['offers']), 1)
         self.assertEqual(i['offers'][0]['seller'], 'George Mason University')
        
 
