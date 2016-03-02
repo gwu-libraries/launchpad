81ce829d10af608a15cd8bf8a6782c81b3176400
Laura Wrubel <lwrubel@gwu.edu>
2016-03-02 16:00:02 -0500

    correcting issue with checkbox field

--- lp/ui/forms.py
+++ lp/ui/forms.py
@@ -12,6 +12,11 @@ class PrintRequestForm(forms.Form):
         ('Other', 'Other'),
         )
 
+    HOLD_CHOICES = (
+        ("Hold for me at the Gelman Library Check Out desk", "Request book be held at Gelman Library Check Out Desk."),
+        # ("Shelve", "Don't hold book for me. Shelve book in library stacks."),
+        )
+
     entry_994442820 = forms.CharField(label="Title", widget=forms.HiddenInput)
     entry_1457763040 = forms.CharField(label="ISBN", widget=forms.HiddenInput)
     entry_1696606454 = forms.CharField(label="Requestor Name",
@@ -20,14 +25,18 @@ class PrintRequestForm(forms.Form):
                                         required=True,
                                         choices=STATUS_CHOICES)
     entry_571937374 = forms.CharField(label="Course, Department, or Program",
-                                      widget=forms.TextInput(attrs={'size':'40'}),
+                                      widget=forms.TextInput(
+                                          attrs={'size': '40'}),
                                       required=True,
                                       help_text='Be as specific as possible.')
     entry_700519383 = forms.EmailField(label="Email Address",
                                        required=True)
     entry_1537829419 = forms.CharField(label="BIBID", widget=forms.HiddenInput)
-    entry_628191412 = forms.BooleanField(label="Request book be held at Gelman Library Check Out Desk",
-                                         initial=True,
-                                         required=False)
+    entry_628191412 = forms.MultipleChoiceField(
+                          widget=forms.CheckboxSelectMultiple(),
+                          required=False,
+                          initial="Hold for me at the Gelman Library Check Out desk",
+                          choices=HOLD_CHOICES,
+                          label="")
     title = forms.CharField(widget=forms.HiddenInput, required=False)
     isbn = forms.CharField(widget=forms.HiddenInput, required=False)
